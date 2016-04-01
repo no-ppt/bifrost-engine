75e82f96ba2e6398dab6ca463141cf2c330e0c28
luochuan <hermit@no-ppt.com>
2016-04-01 17:04:27 +0800

    Package three.js, tween.js and babel-polyfill into bifrost-engine

--- build/build.js
+++ build/build.js
@@ -1,15 +1,15 @@
-/*
- * Copyright © 2016 by Beijing Brilliant Demonstration Technology Co., Ltd. All right reserved.
- */
-
 ({
-    baseUrl    : '../dist',
-    optimize   : 'none',
-    name       : 'Bifrost',
-    paths      : {
-        'three': '../node_modules/three/three',
-        'tween': '../node_modules/tween.js/src/Tween'
+    baseUrl : '../dist',
+    optimize: 'none',
+    name    : 'Bifrost',
+    paths   : {
+        'three'         : '../node_modules/three/three',
+        'tween'         : '../node_modules/tween.js/src/Tween',
+        'babel-polyfill': '../node_modules/babel-polyfill/dist/polyfill'
     },
-    stubModules: [ 'three', 'tween' ],
-    out        : '../dist/bifrost-engine.js'
+    shim    : {
+        'Bifrost': [ 'babel-polyfill', 'three', 'tween' ]
+    },
+    wrapShim: true,
+    out     : '../dist/bifrost-engine.js'
 })
